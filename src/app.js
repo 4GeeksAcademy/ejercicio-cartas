ea48343558a757406ce8c22c74121f2423ccdfe2
Miquel Arnaiz Fayos <arnaizmiquel@gmail.com>
2026-05-21 18:03:38 +0200

    Add card randomization functionality in app.js

---
+++ src/app.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+  const Numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  const Palo = ["Diamantes", "Treboles", "Corazones", "Picas"];
+
+  const randomNumero = Math.floor(Math.random() * Numero.length);
+  const randomPalo = Math.floor(Math.random() * Palo.length);
+
+  document.getElementById("numero").innerHTML = Numero[randomNumero];
+
+  function ObtenerPalo(palo) {
+    switch (palo) {
+      case "Diamantes": return "diamantes";
+      case "Picas": return "picas";
+      case "Corazones": return "corazones";
+      case "Treboles": return "treboles";
+    }
+  }
+
+  const claseElegida = ObtenerPalo(Palo[randomPalo]);
+
+  document.querySelectorAll(".palo").forEach((paloElement) => {
+    paloElement.classList.add(claseElegida);
+  });
+};
